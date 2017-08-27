ce3605719f332a4aa4e3ea9bf6e835203c806a3b
Urs Meier <urs@Urs-iMac.local>
2017-08-27 20:57:17 +0200

    Split View and Icons for Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,15 @@ export class MyApp {
 
   rootPage: any = QuestionairesPage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any, icon:string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Questionaires', component: QuestionairesPage },
-      { title: 'Questions', component: QuestionsPage }
+      { title: 'Questionaires', component: QuestionairesPage, icon:'list' },
+      { title: 'Questions', component: QuestionsPage, icon: 'help' }
     ];
 
   }
